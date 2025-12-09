ad98c9f59ddab347bc33acde927a6c1d7809bf37
hirano-kazuma <hirano.kazuma@moneyforward.co.jp>
2025-12-09 13:30:47 +0900

    FP and general users can now register

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,11 @@ module ApplicationHelper
       "#{page_title} | #{base_title}"
     end
   end
+
+  # 指定されたユーザーのGravatar画像を返す
+  def gravatar_for(user, size: 80)
+    gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+    gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+    image_tag(gravatar_url, alt: user.name, class: "gravatar rounded-circle")
+  end
 end
