8e62838da4a12c700382177bab1f80ea1912d867
hirano-kazuma <hirano.kazuma@moneyforward.co.jp>
2025-12-16 15:22:55 +0900

    delete user status update file

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,35 +5,4 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
-
-  def edit
-    @user = User.find(params[:id])
-  end
-
-  def update
-    @user = User.find(params[:id])
-    if @user.update(user_params)
-      redirect_to @user, notice: "ユーザー情報が更新されました"
-    else
-      render :edit, status: :unprocessable_entity
-    end
-  end
-
-  private
-
-  def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
-  end
-
-  def logged_in_user
-    unless logged_in?
-      flash[:danger] = "ログインが必要です"
-      redirect_to new_session_path
-    end
-  end
-
-  def correct_user
-    @user = User.find(params[:id])
-    redirect_to(root_url) unless current_user?(@user)
-  end
 end
